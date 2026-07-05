92090bf9c5229e3f115461932fdb59ba045f4f12
jinhan3884 <jinhan3884@gmail.com>
2026-07-05 10:15:58 +0900

    Remove conflicting plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{
-        src: 'node_modules/cesium/Build/Cesium/*',
-        dest: 'cesium'
-      }]
-    })
-  ],
+  plugins: [react()],
   define: {
-    CESIUM_BASE_URL: JSON.stringify('/cesium')
+    CESIUM_BASE_URL: JSON.stringify('')
   }
 })
